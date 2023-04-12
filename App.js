f0de404dcabb283663c05881a08b711e9c704536
freddxworld <98077586+freddxworld@users.noreply.github.com>
2023-04-12 14:57:08 -0700

    started working on the main page

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js toooooto start working on your app!</Text>
-      <Text>testing 123 i wanna see if your running correctly </Text>
-      <StatusBar style="auto" />
+      <Text>Banner</Text>
+      <TouchableOpacity style={{ backgroundColor: "blue" }}>
+        <Text>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{ backgroundColor: "blue" }}>
+        <Text>Contact Us</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{ backgroundColor: "blue" }}>
+        <Text>Appointment</Text>
+      </TouchableOpacity>
+      <TextInput
+        style={{
+          height: 25,
+          borderColor: "black",
+          borderWidth: 1,
+          paddingLeft: 15,
+        }}
+        defaultValue="Search"
+      />
+      <Text>Book Appointment</Text>
+      <Text>Email Sign up</Text>
     </View>
+    /*
+    white, red, gray , and dark gray color scheme
+    goes in the view at the very bottom however im commenting it out cause i dont think we need this it seems more used for iphone or android apps
+      <StatusBar style="auto" />
+    */
   );
 }
 
