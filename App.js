88b201ddc0c460669359b3eccbfc0d5013d9d124
freddxworld <98077586+freddxworld@users.noreply.github.com>
2023-04-13 15:37:30 -0700

    updates to the layout

--- App.js
+++ App.js
@@ -1,42 +1,44 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Banner</Text>
-      <TouchableOpacity style={{ backgroundColor: "blue" }}>
-        <Text>Home</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={{ backgroundColor: "blue" }}>
-        <Text>Contact Us</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={{ backgroundColor: "blue" }}>
-        <Text>Appointment</Text>
-      </TouchableOpacity>
-      <TextInput
-        style={{
-          height: 25,
-          borderColor: "black",
-          borderWidth: 1,
-          paddingLeft: 15,
-        }}
-        defaultValue="Search"
-      />
-      <Text>Book Appointment</Text>
-      <Text>Email Sign up</Text>
-    </View>
-    /*
-    white, red, gray , and dark gray color scheme
-    goes in the view at the very bottom however im commenting it out cause i dont think we need this it seems more used for iphone or android apps
+      <View style={styles.banner}>
+        <Text>Banner</Text>
+      </View>
+      <View style={styles.topRow}>
+        <TouchableOpacity style={styles.navButton}>
+          <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navButton}>
+          <Text>Contact Us</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navButton}>
+          <Text>Appointment</Text>
+        </TouchableOpacity>
+        <TextInput
+          placeholder="Search"
+          style={styles.searchInput}
+          defaultValue="Search"
+        />
+      </View>
+      <View style={styles.middleRow}>
+        <Text>Book Appointment</Text>
+      </View>
+      <View style={styles.bottomRow}>
+        <Text>Email Sign up</Text>
+      </View>
       <StatusBar style="auto" />
-    */
+    </View>
   );
 }
 
@@ -46,5 +48,37 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  banner: {
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  topRow: {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr 1fr auto",
+    gridGap: 10,
+    alignItems: "center",
+    padding: 10,
+  },
+  middleRow: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bottomRow: {
+    padding: 10,
+    paddingBottom: 100,
+  },
+  navButton: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  searchInput: {
+    height: 25,
+    borderColor: "black",
+    borderWidth: 1,
+    paddingLeft: 15,
   },
 });
