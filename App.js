267b6a3606ca3d8878ae65c53cd5125490382452
freddxworld <98077586+freddxworld@users.noreply.github.com>
2023-04-17 09:56:42 -0700

    added comments and start adding the about and email sign up

--- App.js
+++ App.js
@@ -8,14 +8,20 @@ import {
   TouchableOpacity,
   Image,
   ScrollView,
+  Dimensions,
 } from "react-native";
+
+const screenHeight = Dimensions.get("screen").height;
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/*this is where we have like images that play like a slideshow */}
       <View style={styles.banner}>
         <Text>Banner</Text>
       </View>
+
+      {/*this is where we will have the top row which will contain all the usefull infor like home, the logo, contact us, appointments */}
       <View style={styles.topRow}>
         <Image
           style={styles.logoImage}
@@ -36,11 +42,26 @@ export default function App() {
           defaultValue="Search"
         />
       </View>
+
+      {/*this is where we have the book appointment with the calendar and all that stuff */}
       <View style={styles.middleRow}>
         <Text>Book Appointment</Text>
       </View>
+
+      {/*this is where we have the bottom row where they can sign up for email and a lil about us page */}
       <View style={styles.bottomRow}>
-        <Text>Email Sign up</Text>
+        <Text style={styles.text}>
+          Welcome to America's Best Chatsworth in Chatsworth, CA Since 1990,
+          America's Best Karate has been providing instruction in Tang Soo Do,
+          Krav Maga and the martial arts in the Chatsworth, CA area. We hope you
+          enjoy our website and will stop by and visit us if you are in the
+          area. This site is designed to give you information about our
+          facility, our style of martial arts, our staff and provide you with
+          information on how to contact us as well.
+        </Text>
+        <View style={styles.Email}>
+          <Text style={styles.text}>Email Sign up</Text>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -54,6 +75,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
+  Email: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+  },
+
   logoImage: {
     color: "Black",
     fontWeight: "bold",
@@ -62,15 +92,18 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     paddingLeft: 80,
   },
+
   textButtons: {
     color: "white",
     fontWeight: "bold",
     fontSize: 20,
   },
+
   banner: {
     paddingTop: 20,
     paddingBottom: 20,
   },
+
   topRow: {
     display: "grid",
     gridTemplateColumns: "2fr 2fr 2fr 2fr 0.5fr",
@@ -82,6 +115,7 @@ const styles = StyleSheet.create({
     marginBottom: "3%",
     height: 75,
   },
+
   middleRow: {
     flex: 1,
     justifyContent: "center",
@@ -90,20 +124,42 @@ const styles = StyleSheet.create({
     marginBottom: "3%",
     width: "50%",
   },
+
   bottomRow: {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    gridGap: 0,
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#c33",
+    width: "100%",
+    marginBottom: "3%",
+    height: "30%",
+    /*
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "lightgray",
     marginBottom: "3%",
     width: "50%",
+    */
   },
+
+  text: {
+    alignItems: "center",
+    color: "white",
+    width: "70%",
+    marginLeft: 40,
+    fontSize: 17,
+  },
+
   navButton: {
     backgroundColor: "#c33",
     padding: 10,
     borderRadius: 5,
     alignItems: "center",
   },
+
   searchInput: {
     height: 25,
     borderColor: "white",
